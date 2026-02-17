45bae807d92e74d10b2a818d07f33db92c278bf7
Mustafa Yekta Taraf <mustafayektataraf@gmail.com>
2026-02-17 20:01:14 +0300

    Update the Settings Section

--- WeatherApp/SettingsController.swift
+++ WeatherApp/SettingsController.swift
@@ -9,6 +9,10 @@ import Foundation
 import UIKit
 
 class SettingsController: UIViewController {
+    
+    let sectionsTitle = [NSLocalizedString("LANGUAGE", comment: "Dil"), NSLocalizedString("MODE", comment: "Mod"), NSLocalizedString("ABOUT_US", comment: "Hakkımızda") ,NSLocalizedString("PRIVACY_POLICY", comment: "Gizlilik Politikası"), NSLocalizedString("RATE_US", comment: "Bizi Puanla")]
+    let sectionsText = [[NSLocalizedString("CHANGE_LANGUAGE", comment: "Dil Değiştir")], [NSLocalizedString("CHANGE_MODE", comment: "Mod Değiştir")], [NSLocalizedString("ABOUT_US", comment: "Hakkımızda")], [NSLocalizedString("PRIVACY_POLICY_TEXT", comment: "Gizlilik Politikamız")],[NSLocalizedString("RATE_US_TEXT", comment: "Bizi Puanlayın ⭐️")]]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -16,235 +20,75 @@ class SettingsController: UIViewController {
         setupUI()
     }
     
-    private let languageTitle: UILabel = {
-        let label = UILabel()
-        label.text = NSLocalizedString("LANGUAGE", comment: "Dil")
-        label.textAlignment = .center
-        label.font = UIFont.systemFont(ofSize: 20, weight: .bold)
-        return label
+    private let settingsTableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .insetGrouped)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "settingsCell")
+        tableView.backgroundColor = .clear
+        return tableView
     }()
     
-    private let languageLabel: UILabel = {
-        let label = UILabel()
-        label.text = NSLocalizedString("CHANGE_LANGUAGE", comment: "Dil Değiştir")
-        label.textAlignment = .center
-        label.font = UIFont.systemFont(ofSize: 16, weight: .regular)
-        return label
-    }()
-    
-    private let languageView: UIView = {
-        let languageView = UIView()
-        languageView.backgroundColor = .init(named: "CellsSystemGray")
-        languageView.layer.cornerRadius = 16
-        languageView.layer.masksToBounds = true
-        return languageView
-    }()
-    
-    private let modeTitle: UILabel = {
-        let label = UILabel()
-        label.text = NSLocalizedString("MODE", comment: "Mod")
-        label.textAlignment = .center
-        label.font = UIFont.systemFont(ofSize: 20, weight: .bold)
-        return label
-    }()
-    
-    private let modeView: UIView = {
-        let modeView = UIView()
-        modeView.backgroundColor = .init(named: "CellsSystemGray")
-        modeView.layer.cornerRadius = 16
-        modeView.layer.masksToBounds = true
-        return modeView
-    }()
-    
-    private let modeLabel: UILabel = {
-        let label = UILabel()
-        label.text = NSLocalizedString("CHANGE_MODE", comment: "Mod Değiştir")
-        label.textAlignment = .center
-        label.font = UIFont.systemFont(ofSize: 16, weight: .regular)
-        return label
-    }()
-    
-    private let modeSwitch: UISwitch = {
-        let modeSwitch = UISwitch()
-        return modeSwitch
-    }()
-    
-    private let privacyPolicyTitle: UILabel = {
-        let label = UILabel()
-        label.text = NSLocalizedString("PRIVACY_POLICY", comment: "Gizlilik Politikası")
-        label.textAlignment = .center
-        label.font = UIFont.systemFont(ofSize: 20, weight: .bold)
-        return label
-    }()
-    
-    private let privacyPolicyView: UIView = {
-        let privacyPolicyView = UIView()
-        privacyPolicyView.backgroundColor = .init(named: "CellsSystemGray")
-        privacyPolicyView.layer.cornerRadius = 16
-        privacyPolicyView.layer.masksToBounds = true
-        return privacyPolicyView
-    }()
-    
-    private let privacyPolicyLabel: UILabel = {
-        let label = UILabel()
-        label.text = NSLocalizedString("PRIVACY_POLICY_TEXT", comment: "Gizlilik Politikamız")
-        label.textAlignment = .center
-        label.numberOfLines = 0
-        label.font = UIFont.systemFont(ofSize: 16, weight: .regular)
-        return label
-    }()
-    
-    private let rateUsTitle: UILabel = {
-        let label = UILabel()
-        label.text = NSLocalizedString("RATE_US", comment: "Bizi Puanla")
-        label.textAlignment = .center
-        label.font = UIFont.systemFont(ofSize: 20, weight: .bold)
-        return label
-    }()
-    
-    private let rateUsView: UIView = {
-        let rateUsView = UIView()
-        rateUsView.backgroundColor = .init(named: "CellsSystemGray")
-        rateUsView.layer.cornerRadius = 16
-        rateUsView.layer.masksToBounds = true
-        return rateUsView
-    }()
-    
-    private let rateUsLabel: UILabel = {
-        let label = UILabel()
-        label.text = NSLocalizedString("RATE_US_TEXT", comment: "Bizi Puanlayın ⭐️")
-        label.textAlignment = .center
-        label.numberOfLines = 0
-        label.font = UIFont.systemFont(ofSize: 16, weight: .regular)
-        return label
-    }()
-    
-    private func setupUI(){
-        view.addSubview(languageTitle)
+    @objc func changeModeSwitchTapped(_ sender: UISwitch){
+        let style: UIUserInterfaceStyle = sender.isOn ? .dark : .light
         
-        languageTitle.translatesAutoresizingMaskIntoConstraints = false
+        if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene {
+            windowScene.windows.forEach { window in
+                window.overrideUserInterfaceStyle = style
+            }
+        }
+        UserDefaults.standard.set(sender.isOn, forKey: "isDarkMode")
+    }
+    
+    private func setupUI() {
+        view.addSubview(settingsTableView)
+        settingsTableView.delegate = self
+        settingsTableView.dataSource = self
+        
+        settingsTableView.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-            languageTitle.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30),
-            languageTitle.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 30)
-            ])
-        
-        view.addSubview(languageView)
-        
-        languageView.translatesAutoresizingMaskIntoConstraints = false
-        
-        NSLayoutConstraint.activate([
-            languageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 60),
-            languageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
-            languageView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
-            languageView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 115)
+            settingsTableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30),
+            settingsTableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -30),
+            settingsTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 5),
+            settingsTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -5),
         ])
+    }
+    
+}
+
+extension SettingsController: UITableViewDataSource, UITableViewDelegate {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return sectionsTitle.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sectionsText[section].count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "settingsCell", for: indexPath)
+        cell.textLabel?.text = sectionsText[indexPath.section][indexPath.row]
+        cell.backgroundColor = .init(named: "CellsSystemGray")
         
-        view.addSubview(languageLabel)
+        if indexPath.section == 1 {
+            let switchView = UISwitch()
+            
+            let isDarkMode = UserDefaults.standard.bool(forKey: "isDarkMode")
+            switchView.isOn = isDarkMode
+            
+            switchView.addTarget(self, action: #selector(changeModeSwitchTapped), for: .valueChanged)
+            
+            cell.selectionStyle = .none
+            cell.accessoryView = switchView
+        } else {
+            cell.accessoryView = nil
+            cell.selectionStyle = .default
+        }
         
-        languageLabel.translatesAutoresizingMaskIntoConstraints = false
-        
-        NSLayoutConstraint.activate([
-            languageLabel.centerYAnchor.constraint(equalTo: languageView.centerYAnchor),
-            languageLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 30)
-        ])
-        
-        view.addSubview(modeTitle)
-        
-        modeTitle.translatesAutoresizingMaskIntoConstraints = false
-        
-        NSLayoutConstraint.activate([
-            modeTitle.topAnchor.constraint(equalTo: languageView.bottomAnchor, constant: 30),
-            modeTitle.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 30)
-        ])
-        
-        view.addSubview(modeView)
-        
-        modeView.translatesAutoresizingMaskIntoConstraints = false
-        
-        NSLayoutConstraint.activate([
-            modeView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 180),
-            modeView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
-            modeView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
-            modeView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 235)
-            ])
-        
-        view.addSubview(modeLabel)
-        
-        modeLabel.translatesAutoresizingMaskIntoConstraints = false
-        
-        NSLayoutConstraint.activate([
-            modeLabel.centerYAnchor.constraint(equalTo: modeView.centerYAnchor),
-            modeLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 30)
-        ])
-        
-        view.addSubview(modeSwitch)
-        
-        modeSwitch.translatesAutoresizingMaskIntoConstraints = false
-        
-        NSLayoutConstraint.activate([
-            modeSwitch.centerYAnchor.constraint(equalTo: modeView.centerYAnchor),
-            modeSwitch.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -30)
-        ])
-        
-        view.addSubview(privacyPolicyTitle)
-        
-        privacyPolicyTitle.translatesAutoresizingMaskIntoConstraints = false
-        
-        NSLayoutConstraint.activate([
-            privacyPolicyTitle.topAnchor.constraint(equalTo: modeView.bottomAnchor, constant: 30),
-            privacyPolicyTitle.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 30)
-        ])
-        
-        view.addSubview(privacyPolicyView)
-        
-        privacyPolicyView.translatesAutoresizingMaskIntoConstraints = false
-        
-        NSLayoutConstraint.activate([
-            privacyPolicyView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 300),
-            privacyPolicyView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
-            privacyPolicyView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
-            privacyPolicyView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 355)
-            ])
-        
-        view.addSubview(privacyPolicyLabel)
-        
-        privacyPolicyLabel.translatesAutoresizingMaskIntoConstraints = false
-        
-        NSLayoutConstraint.activate([
-            privacyPolicyLabel.centerYAnchor.constraint(equalTo: privacyPolicyView.centerYAnchor),
-            privacyPolicyLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 30)
-        ])
-        
-        view.addSubview(rateUsTitle)
-        
-        rateUsTitle.translatesAutoresizingMaskIntoConstraints = false
-        
-        NSLayoutConstraint.activate([
-            rateUsTitle.topAnchor.constraint(equalTo: privacyPolicyView.bottomAnchor, constant: 30),
-            rateUsTitle.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 30)
-        ])
-        
-        view.addSubview(rateUsView)
-        
-        rateUsView.translatesAutoresizingMaskIntoConstraints = false
-        
-        NSLayoutConstraint.activate([
-            rateUsView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 420),
-            rateUsView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
-            rateUsView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
-            rateUsView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 475)
-            ])
-        
-        view.addSubview(rateUsLabel)
-        
-        rateUsLabel.translatesAutoresizingMaskIntoConstraints = false
-        
-        NSLayoutConstraint.activate([
-            rateUsLabel.centerYAnchor.constraint(equalTo: rateUsView.centerYAnchor),
-            rateUsLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 30)
-        ])
-        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sectionsTitle[section]
     }
     
 }
