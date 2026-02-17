3070ebb40224571ec8afb9311ac1b6dfc0b803fe
Mustafa Yekta Taraf <mustafayektataraf@gmail.com>
2026-02-17 12:17:46 +0300

    Update the App

---
+++ WeatherApp/HomeController.swift
@@ -0,0 +1,16 @@
+//
+//  HomeController.swift
+//  WeatherApp
+//
+//  Created by yekta on 17.02.2026.
+//
+
+import Foundation
+import UIKit
+
+class HomeController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
---
+++ WeatherApp/TabBarController.swift
@@ -0,0 +1,22 @@
+//
+//  TabBarController.swift
+//  WeatherApp
+//
+//  Created by yekta on 17.02.2026.
+//
+
+import Foundation
+import UIKit
+
+class TabBarController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    private func setupTabBar() {
+        let 
+    }
+    
+}
